9d2a04161d3f40796c75652d02f80738b987db6a
pointwind <503652093@qq.com>
2022-05-16 00:56:27 +0800

    all issue in github

--- src/main/java/com/cs209/project/script/BugNLP.java
+++ src/main/java/com/cs209/project/script/BugNLP.java
@@ -1,122 +1,61 @@
 package com.cs209.project.script;
 
-import edu.stanford.nlp.coref.data.CorefChain;
-import edu.stanford.nlp.ling.*;
-import edu.stanford.nlp.ie.util.*;
-import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.semgraph.*;
-import edu.stanford.nlp.trees.*;
-import java.util.*;
+import  org.apache.commons.httpclient.Cookie;
+import  org.apache.commons.httpclient.HttpClient;
+import  org.apache.commons.httpclient.NameValuePair;
+import  org.apache.commons.httpclient.cookie.CookiePolicy;
+import  org.apache.commons.httpclient.methods.GetMethod;
+import  org.apache.commons.httpclient.methods.PostMethod;
 
+class  HttpLogin {
 
-public class BugNLP {
+    public  static  void  main(String[] args) {
+        // 登陆 Url
+        String loginUrl =  "https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f" ;
+        // 需登陆后访问的 Url
+        String dataUrl =  "https://api.stackexchange.com/2.3/tags/spring-boot/faq?page=25&pagesize=100&site=stackoverflow" ;
+        HttpClient httpClient =  new  HttpClient();
 
-    public static String text = "Joe Smith was born in California. " +
-            "In 2017, he went to Paris, France in the summer. " +
-            "His flight left at 3:00pm on July 10th, 2017. " +
-            "After eating some escargot for the first time, Joe said, \"That was delicious!\" " +
-            "He sent a postcard to his sister Jane Smith. " +
-            "After hearing about Joe's trip, Jane decided she might go to France one day.";
+        // 模拟登陆，按实际服务器端要求选用 Post 或 Get 请求方式
+        PostMethod postMethod =  new  PostMethod(loginUrl);
 
-    public static void main(String[] args) {
-        // set up pipeline properties
-        Properties props = new Properties();
-        // set the list of annotators to run
-        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref,kbp,quote");
-        // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
-        props.setProperty("coref.algorithm", "neural");
-        // build pipeline
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        // create a document object
-        CoreDocument document = new CoreDocument(text);
-        // annnotate the document
-        pipeline.annotate(document);
-        // examples
+        // 设置登陆时要求的信息，用户名和密码
+        NameValuePair[] data = {  new  NameValuePair( "loginName" ,  "12012211@mail.sustech.edu.cn" ),  new  NameValuePair( "loginPasswd" ,  "chenqiujiang0904" ) };
+        postMethod.setRequestBody(data);
+        try  {
+            // 设置 HttpClient 接收 Cookie,用与浏览器一样的策略
+            httpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
+            int  statusCode=httpClient.executeMethod(postMethod);
 
-        // 10th token of the document
-        CoreLabel token = document.tokens().get(10);
-        System.out.println("Example: token");
-        System.out.println(token);
-        System.out.println();
-
-        // text of the first sentence
-        String sentenceText = document.sentences().get(0).text();
-        System.out.println("Example: sentence");
-        System.out.println(sentenceText);
-        System.out.println();
-
-        // second sentence
-        CoreSentence sentence = document.sentences().get(1);
-
-        // list of the part-of-speech tags for the second sentence
-        List<String> posTags = sentence.posTags();
-        System.out.println("Example: pos tags");
-        System.out.println(posTags);
-        System.out.println();
-
-        // list of the ner tags for the second sentence
-        List<String> nerTags = sentence.nerTags();
-        System.out.println("Example: ner tags");
-        System.out.println(nerTags);
-        System.out.println();
-
-        // constituency parse for the second sentence
-        Tree constituencyParse = sentence.constituencyParse();
-        System.out.println("Example: constituency parse");
-        System.out.println(constituencyParse);
-        System.out.println();
-
-        // dependency parse for the second sentence
-        SemanticGraph dependencyParse = sentence.dependencyParse();
-        System.out.println("Example: dependency parse");
-        System.out.println(dependencyParse);
-        System.out.println();
-
-        // kbp relations found in fifth sentence
-        List<RelationTriple> relations =
-                document.sentences().get(4).relations();
-        System.out.println("Example: relation");
-        System.out.println(relations.get(0));
-        System.out.println();
-
-        // entity mentions in the second sentence
-        List<CoreEntityMention> entityMentions = sentence.entityMentions();
-        System.out.println("Example: entity mentions");
-        System.out.println(entityMentions);
-        System.out.println();
-
-        // coreference between entity mentions
-        CoreEntityMention originalEntityMention = document.sentences().get(3).entityMentions().get(1);
-        System.out.println("Example: original entity mention");
-        System.out.println(originalEntityMention);
-        System.out.println("Example: canonical entity mention");
-        System.out.println(originalEntityMention.canonicalEntityMention().get());
-        System.out.println();
-
-        // get document wide coref info
-        Map<Integer, CorefChain> corefChains = document.corefChains();
-        System.out.println("Example: coref chains for document");
-        System.out.println(corefChains);
-        System.out.println();
-
-        // get quotes in document
-        List<CoreQuote> quotes = document.quotes();
-        CoreQuote quote = quotes.get(0);
-        System.out.println("Example: quote");
-        System.out.println(quote);
-        System.out.println();
-
-        // original speaker of quote
-        // note that quote.speaker() returns an Optional
-        System.out.println("Example: original speaker of quote");
-        System.out.println(quote.speaker().get());
-        System.out.println();
-
-        // canonical speaker of quote
-        System.out.println("Example: canonical speaker of quote");
-        System.out.println(quote.canonicalSpeaker().get());
-        System.out.println();
-
+            // 获得登陆后的 Cookie
+            Cookie[] cookies = httpClient.getState().getCookies();
+            StringBuilder tmpcookies =  new StringBuilder();
+            for  (Cookie c : cookies) {
+                tmpcookies.append(c.toString()).append(";");
+                System.out.println( "cookies = " + c);
+            }
+//            if (statusCode== 302 ){ //重定向到新的URL
+                System.out.println( "success" );
+                // 进行登陆后的操作
+                GetMethod getMethod =  new  GetMethod(dataUrl);
+                // 每次访问需授权的网址时需带上前面的 cookie 作为通行证
+                getMethod.setRequestHeader( "cookie" , tmpcookies.toString());
+                // 你还可以通过 PostMethod/GetMethod 设置更多的请求后数据
+                // 例如，referer 从哪里来的，UA 像搜索引擎都会表名自己是谁，无良搜索引擎除外
+                postMethod.setRequestHeader( "Referer" ,  "https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f" );
+                postMethod.setRequestHeader( "User-Agent" ,  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36" );
+                httpClient.executeMethod(getMethod);
+                // 打印出返回数据，检验一下是否成功
+                System.out.println(httpClient.getState());
+                String text = getMethod.getResponseBodyAsString();
+                System.out.println(text);
+//            }
+//            else  {
+//                System.out.println( "登录失败" );
+//            }
+        }
+        catch  (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
