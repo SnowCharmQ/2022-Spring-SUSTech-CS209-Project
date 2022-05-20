ce1e3753aeb9604acebb65cb19dec65a63aaf552
SnowCharm <619022098@qq.com>
2022-05-21 01:08:15 +0800

    update question.js

--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -1,11 +1,50 @@
-function selectQuestion() {
+function selectSpringBootQuestion() {
     $.ajax({
-        url: "/springboot/question",
+        url: "/springboot/springboot-question",
         type: 'POST',
-        data: $("#select-question").serialize(),
+        data: $("#springboot-question").serialize(),
         dataType: "json",
         success: function (json) {
-            var ele = $(".panel")
+            var ele = $(".springboot-panel")
+            ele.empty()
+            if (json.state === 200) {
+                var head = '<div class="head">' +
+                    '        <span class="question-title">Question</span>' +
+                    '        <span class="date-title">Date</span>' +
+                    '        <span class="answers-title">Answers</span>' +
+                    '        <span class="views-title">Views</span>' +
+                    '        <span class="link-title">Link</span>' +
+                    '    </div>' +
+                    '    <div class="spacing">none</div>'
+                ele.append(head)
+                var list = json.data;
+                for (var i = 0; i < list.length; i++) {
+                    var temp = list[i];
+                    var row = '<div class="row">' +
+                        '        <span class="question">' + temp.question + '</span>' +
+                        '        <span class="date">' + temp.date + '</span>' +
+                        '        <span class="views">' + temp.views + '</span>' +
+                        '        <span class="answers">' + temp.answers + '</span>' +
+                        '        <a href="' + temp.href + '"' +
+                        '           class="link">The Link To This QUESTION</a>' +
+                        '    </div>'
+                    ele.append(row)
+                }
+            } else if (json.state === 220) {
+                ele.append("<h2>Sorry! No Data Was Found!</h2>")
+            }
+        }
+    })
+}
+
+function selectMyBatisQuestion() {
+    $.ajax({
+        url: "/mybatis/mybatis-question",
+        type: 'POST',
+        data: $("#mybatis-question").serialize(),
+        dataType: "json",
+        success: function (json) {
+            var ele = $(".mybatis-panel")
             ele.empty()
             if (json.state === 200) {
                 var head = '<div class="head">' +
