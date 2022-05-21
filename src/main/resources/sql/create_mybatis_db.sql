36738e7faf6fac884b13667e1984245313fbd9a1
SnowCharm <619022098@qq.com>
2022-05-21 17:03:53 +0800

    create table mybatis_issue

--- src/main/resources/sql/create_mybatis_db.sql
+++ src/main/resources/sql/create_mybatis_db.sql
@@ -6,3 +6,10 @@ create table if not exists mybatis_question
     answers  integer,
     href     varchar(200)
 );
+
+create table if not exists mybatis_issue(
+    version varchar(15),
+    date date,
+    year integer,
+    info varchar(300)
+);
