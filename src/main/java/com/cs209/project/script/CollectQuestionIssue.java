80d4c1c01c380e6d89be6df5a2cf3046e1da6cd9
pointwind <503652093@qq.com>
2022-05-23 20:06:30 +0800

    all issue in github

--- src/main/java/com/cs209/project/script/CollectQuestionIssue.java
+++ src/main/java/com/cs209/project/script/CollectQuestionIssue.java
@@ -4,14 +4,14 @@ import java.io.*;
 
 public class CollectQuestionIssue {
     public static void main(String[]args) throws IOException {
-        File f = new File("src/test/java/com/cs209/project/file/MybatisOpenIssueDetail.txt");
+        File f = new File("src/test/java/com/cs209/project/file/SpringBootStackoverflowQuestionDetail.txt");
         String s;
         BufferedReader br = new BufferedReader(new FileReader(f));
-        File ff = new File("src/test/java/com/cs209/project/file/MybatisIssuefile.txt");
+        File ff = new File("src/test/java/com/cs209/project/file/SpringBootStackoverflowQuestionZ.txt");
         BufferedWriter bw = new BufferedWriter(new FileWriter(ff));
         String last = "";
         while((s = br.readLine())!=null){
-            String parts = s.split("\t")[2];
+            String parts = s.split("\t")[0];
             if(!last.equals(parts)){
                 bw.write(parts);
                 bw.write("\n");
